3254ca07b308797d7816e5ebba1b111ef806bc08
Mehmet Yağcı <mehmetyagci53@gmail.com>
2021-08-10 15:24:53 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started.");
+
+            Console.WriteLine("Ended.");
+
+            Console.ReadLine();
+
+        } // end of main
+
+    } // end of class
+} // end of namespace
